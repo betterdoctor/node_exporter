4e052fba516164e2661c67d4907d700a6eec508b
Steve Best <steve@betterdoctor.com>
2017-07-10 12:31:00 -0700

    SYS-2106 - Fixed logging for systemd

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@betterdoctor.com'
 license          'Apache 2.0'
 description      'Installs node_exporter'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.14'
+version          '0.0.16'
 source_url       'https://github.com/betterdoctor/node_exporter'
 issues_url       'https://github.com/betterdoctor/node_exporter/issues'
 chef_version     '>= 12.15.25' if respond_to?(:chef_version)
@@ -13,7 +13,7 @@ chef_version     '>= 12.15.25' if respond_to?(:chef_version)
   supports os
 end
 
-depends 'runit', '~> 1.5'
+depends 'runit', '~> 1.7.8'
 depends 'ark', '~> 2.1.0'
 depends 'compat_resource', '~> 12.19.0'
 
