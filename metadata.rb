dfdd7ac86d4c0593be59f49a5ca02def2b9bfc70
Steve Best <steve@betterdoctor.com>
2017-07-17 11:00:46 -0700

    SYS-2118 - Removed custom logging from services

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@betterdoctor.com'
 license          'Apache 2.0'
 description      'Installs node_exporter'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.18'
+version          '0.0.19'
 source_url       'https://github.com/betterdoctor/node_exporter'
 issues_url       'https://github.com/betterdoctor/node_exporter/issues'
 chef_version     '>= 12.15.25' if respond_to?(:chef_version)
